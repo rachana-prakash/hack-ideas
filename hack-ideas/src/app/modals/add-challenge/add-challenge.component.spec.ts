395df1003e5c1befe265d7db433be35cf5bd22e3
rachana-prakash <rachana.prakash91@gmail.com>
2021-04-25 19:04:49 +0530

    minor test cases

--- hack-ideas/src/app/modals/add-challenge/add-challenge.component.spec.ts
+++ hack-ideas/src/app/modals/add-challenge/add-challenge.component.spec.ts
@@ -7,7 +7,7 @@ import {ReactiveFormsModule} from '@angular/forms';
 describe('AddChallengeComponent', () => {
   let component: AddChallengeComponent;
   let fixture: ComponentFixture<AddChallengeComponent>;
-
+  let challenges = [];
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [RouterTestingModule, ReactiveFormsModule],
@@ -20,9 +20,41 @@ describe('AddChallengeComponent', () => {
     fixture = TestBed.createComponent(AddChallengeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+    challenges = [{
+      id: 1,
+      title: 'Sales by Match',
+      description: 'Test',
+      tag: 'Tech',
+      count: 1,
+      date: new Date()
+    },
+      {
+        id: 2,
+        title: 'Counting Valleys',
+        description: 'Test',
+        tag: 'Tech',
+        count: 2,
+        date: new Date()
+      },
+      {
+        id: 3,
+        title: 'Repeated String',
+        description: 'Test',
+        tag: 'Feature',
+        count: 0,
+        date: new Date()
+      }];
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  describe('addChallenge', () => {
+    it('should increase the challenge array by 1', () => {
+      component.challenges = challenges;
+      component.addChallengeForm.patchValue({title: 'test', description: 'test desc', tag: 'tech'});
+      component.addChallenge();
+      expect(component.challenges.length).toBe(4);
+    });
   });
 });
